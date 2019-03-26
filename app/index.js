5d4955294d98425df1bcf7c7a1d105cf758cd205
Cody Goss <c.ward.goss@gmail.com>
2019-03-25 21:02:57 -0400

    complete react app open run npm start to begin dev

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-require('index.css');
+require('./index.css');
 
 class App extends React.Component {
   render() {
-    return <div>Hello World</div>;
+    return <div>Hello World!</div>;
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('app'));
