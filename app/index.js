92eb46f253570753c1f6244c87d94f291fcc3ea4
Cody Goss <c.ward.goss@gmail.com>
2019-03-25 20:29:05 -0400

    react component complete

--- app/index.js
+++ app/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// state
-// lifecycle events ( allows you to hook into differnt lifecycle moments in a componet )
-//    I want to run this component when data is loads to the DOM
-// UI ( shown with a render method )
-
 class App extends React.Component {
   render() {
     return <div>Hello World</div>;
   }
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
