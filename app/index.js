19fd35106c6611c6f6cde369cfd2edb626b4b7a9
Cody Goss <c.ward.goss@gmail.com>
2019-03-25 20:32:25 -0400

    adds the dependencies

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const React = require('react');
+const ReactDOM = require('react-dom');
+require('index.css');
 
 class App extends React.Component {
   render() {
