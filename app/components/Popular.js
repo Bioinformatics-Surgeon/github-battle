e11773a77b2e043348a52f63b62a960115ab4f44
Cody Goss <c.ward.goss@gmail.com>
2019-04-16 17:33:09 -0400

    makes top nav bar a funtional component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,50 +1,73 @@
 const React = require('react');
+const api = require('../utils/api');
+const PropTypes = require('prop-types');
+
+function SelectedLanguage(props) {
+  let languages = ['All', 'Javascript', 'Python', 'C#', 'Java', 'Ruby', 'CSS'];
+  return (
+    <ul className="languagesNav">
+      {languages.map(function(lang) {
+        return (
+          <li
+            style={
+              lang === props.selectedLanguage ? { color: '#d0021b' } : null
+            }
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}
+          >
+            {lang}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+SelectedLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+};
 
 class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
     // calling updateLanguage in the correct contenxt
     this.updateLanguage = this.updateLanguage.bind(this);
   }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
   updateLanguage(lang) {
     this.setState(function() {
       return {
         selectedLanguage: lang
       };
     });
+    api.fetchPopularRepos(lang).then(
+      function(repos) {
+        this.setState(function(repos) {
+          return {
+            repos: repos
+          };
+        });
+      }.bind(this)
+    );
   }
+
   render() {
-    let languages = [
-      'All',
-      'Javascript',
-      'Python',
-      'C#',
-      'Java',
-      'Ruby',
-      'CSS'
-    ];
     return (
-      <ul className="languagesNav">
-        {this.state.selectedLanguage}
-        {languages.map(function(lang) {
-          return (
-            <li
-              style={
-                lang === this.state.selectedLanguage
-                  ? { color: '#d0021b' }
-                  : null
-              }
-              onClick={this.updateLanguage.bind(null, lang)}
-              key={lang}
-            >
-              {lang}
-            </li>
-          );
-        }, this)}
-      </ul>
+      <div>
+        <SelectedLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+      </div>
     );
   }
 }
---
+++ app/utils/api.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+// all the api request in the app
+
+module.exports = {
+  // get all repos with more than one Star, in a particular Language, sort by Stars, order in decending order, type must be a repository
+  fetchPopularRepos: function(language) {
+    let encodedURI = window.encodeURI(
+      'https://api.github.com/search/repositories?q=starts:>1+language:' +
+        'Java' +
+        '&sort=stars&order=desc&type=Repositories'
+    );
+
+    return axios.get(encodedURI).then(function(response) {
+      return response.data.items;
+    });
+  }
+};
