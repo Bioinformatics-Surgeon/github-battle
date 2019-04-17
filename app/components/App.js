fc746b3f925491a7be77a7d755b10f10577800fa
Cody Goss <c.ward.goss@gmail.com>
2019-04-17 17:02:10 -0400

    app has react router on componets

--- app/components/App.js
+++ app/components/App.js
@@ -3,6 +3,7 @@ const Popular = require('./Popular');
 const ReactRouter = require('react-router-dom');
 const Router = ReactRouter.BrowserRouter;
 const Route = ReactRouter.Route;
+const Switch = require('react-router-dom').Switch;
 const Nav = require('./Nav');
 const Home = require('./Home');
 const Battle = require('./Battle');
@@ -13,9 +14,16 @@ class App extends React.Component {
       <Router>
         <div className='container'>
           <Nav />
-          <Route exact path='/' component={Home} />
-          <Route path='/battle' component={Battle} />
-          <Route path='/popular' component={Popular} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route
+              render={function() {
+                return <p>Not Found</p>;
+              }}
+            />
+          </Switch>
         </div>
       </Router>
     );
