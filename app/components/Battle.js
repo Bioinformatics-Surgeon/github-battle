890b2d600fe9b10f7ab5a32397cda53f448890bf
Cody Goss <c.ward.goss@gmail.com>
2019-04-18 20:20:50 -0400

    adds state to the battle componet

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,9 +1,31 @@
 const React = require('react');
 
 class Battle extends React.Component {
+  constructor() {
+    super(props);
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImg: null,
+      playerTwoImg: null
+    };
+
+    this.handleSumbit = this.handleSumbit.bind(this);
+  }
+
+  handleSumbit(id, username) {
+    this.setState(function() {
+      let newState = {};
+      newState[id + 'Name'] = username;
+    });
+  }
   render() {
-    return <div>Battle!</div>;
+    return <div />;
   }
 }
 
 module.exports = Battle;
+
+// React is controlling the state of the app with forms, this is often called a controlled component
+// Encaplsulation - parent doesn't care about the current input it only cares about the end or final result
+// the child will pass username to the parent with an onSubmit handleler.
